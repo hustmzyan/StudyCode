32742a57c59122fd1194b896ea256338975e0327
lovemz <mengzhao.yan@foxmail.com>
2018-11-02 23:00:11 +0800

    Ex2

---
+++ AdvanceSoftware/Ex2/Ex2/Ex2Dlg.h
@@ -0,0 +1,36 @@
+
+// Ex2Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// CEx2Dlg 对话框
+class CEx2Dlg : public CDialogEx
+{
+// 构造
+public:
+	CEx2Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EX2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	CMenu m_Menu;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
